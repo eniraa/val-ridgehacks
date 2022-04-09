afdfeaceb41930d77db3120fd2a333b433d02a5d
pyxiis <47072520+eniraa@users.noreply.github.com>
2022-04-09 12:59:54 -0400

    feat: add dogfight cover

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -1,42 +1,84 @@
 <section class="bg-gradient-to-r from-slate-900 to-fuchsia-900 min-h-screen">
-    <div class="max-w-7xl min-h-screen mx-auto pt-40">
-    <h1 class="text-pink-100 text-center text-8xl">Welcome to</h1>
-    <svg class="mx-auto h-36 mt-16" width="116mm" height="64mm" version="1.1" viewBox="0 0 116 64" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
-        <metadata>
-         <rdf:RDF>
-          <cc:Work rdf:about="">
-           <dc:format>image/svg+xml</dc:format>
-           <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/>
-           <dc:title/>
-          </cc:Work>
-         </rdf:RDF>
-        </metadata>
-        <g fill="#FCE7F3">
-         <path d="m0 0 5.1676e-4 64 64-64h-7.999l-52 52-5.168e-4 -52z" stroke-width="16"/>
-         <path d="m72 64.001-5.1e-4 -64.001-64 64.001h7.999l52-52.001 5.1e-4 52.001z" stroke-width="16"/>
-         <path transform="scale(.26458)" d="m287.24 0v241.89h151.18v-15.119h-136.06v-226.77z" stroke-width="60.472"/>
-        </g>
-       </svg>
-    <p class="text-center mt-36 text-pink-100 text-3xl">Enter the Valhalla of bots</p></div>
+	<div class="max-w-7xl min-h-screen mx-auto pt-40">
+		<h1 class="text-pink-100 text-center text-8xl">Welcome to</h1>
+		<svg
+			class="mx-auto h-36 mt-16"
+			width="116mm"
+			height="64mm"
+			version="1.1"
+			viewBox="0 0 116 64"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:cc="http://creativecommons.org/ns#"
+			xmlns:dc="http://purl.org/dc/elements/1.1/"
+			xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+		>
+			<metadata>
+				<rdf:RDF>
+					<cc:Work rdf:about="">
+						<dc:format>image/svg+xml</dc:format>
+						<dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+						<dc:title />
+					</cc:Work>
+				</rdf:RDF>
+			</metadata>
+			<g fill="#FCE7F3">
+				<path d="m0 0 5.1676e-4 64 64-64h-7.999l-52 52-5.168e-4 -52z" stroke-width="16" />
+				<path
+					d="m72 64.001-5.1e-4 -64.001-64 64.001h7.999l52-52.001 5.1e-4 52.001z"
+					stroke-width="16"
+				/>
+				<path
+					transform="scale(.26458)"
+					d="m287.24 0v241.89h151.18v-15.119h-136.06v-226.77z"
+					stroke-width="60.472"
+				/>
+			</g>
+		</svg>
+		<p class="text-center mt-36 text-pink-100 text-3xl">Enter the Valhalla of bots</p>
+	</div>
 </section>
-<svg style="background-image: linear-gradient(-90deg, #701a75, #0f172a)" viewBox="0 5 200 15" preserveAspectRatio="none">
-    <defs>
-        <linearGradient id="Gradient1">
-          <stop offset="0%" stop-color="#7dd3fc"/>
-          <stop offset="100%" stop-color="#818cf8"/>
-        </linearGradient>
-    </defs>
-    <path fill="url(#Gradient1)" opacity="0.25" d="M50 30V15Q80 3 110 15V30zM150 30V15Q180 3 210 15V30z"></path>
-    <path fill="url(#Gradient1)" d="M0 30V12Q30 7 55 12T100 13V30zM100 30V13Q130 7 155 12T200 11V30z"></path>
+<svg
+	style="background-image: linear-gradient(-90deg, #701a75, #0f172a)"
+	viewBox="0 5 200 15"
+	preserveAspectRatio="none"
+>
+	<defs>
+		<linearGradient id="Gradient1">
+			<stop offset="0%" stop-color="#7dd3fc" />
+			<stop offset="100%" stop-color="#818cf8" />
+		</linearGradient>
+	</defs>
+	<path
+		fill="url(#Gradient1)"
+		opacity="0.25"
+		d="M50 30V15Q80 3 110 15V30zM150 30V15Q180 3 210 15V30z"
+	/>
+	<path
+		fill="url(#Gradient1)"
+		d="M0 30V12Q30 7 55 12T100 13V30zM100 30V13Q130 7 155 12T200 11V30z"
+	/>
 </svg>
 <section class="bg-gradient-to-r from-sky-300 to-indigo-400 min-h-screen">
-    <div class="max-w-7xl min-h-screen mx-auto pt-20">
-    <h2 class="text-pink-100 text-center text-6xl">Featured Games</h2>
-<div class="relative box-content h-80 w-80 p-8 bg-slate-200 rounded-lg mt-20 z-0">
-    <div class="absolute m-auto h-56 w-full p-8 bg-slate-900 rounded-lg z-10" style="top: 1rem; right:-1rem;"></div>
-    <p class="absolute z-10 top-64 text-lg"><i>Dogfight</i><br>Fly a spaceship and fight others, all while avoiding being shot down yourself.</p>
-</div> 
-
+	<div class="max-w-7xl min-h-screen mx-auto pt-20">
+		<h2 class="text-pink-100 text-center text-6xl">Featured Games</h2>
+		<div class="flex flex-row space-x-16"><a href="/dogfight">
+			<div class="relative box-content h-80 w-80 p-8 bg-slate-200 rounded-lg mt-20 z-0">
+				<div
+					class="absolute m-auto h-56 w-full p-8 rounded-lg z-10"
+					style="top: 1rem; right:-1rem; background-image:url(/dogfight.jpg); background-size: contain;"
+				/>
+				<p class="absolute z-10 top-64 text-lg">
+					<i>Dogfight</i><br /><br />Fly a spaceship and fight others, all while avoiding being shot
+					down yourself.
+				</p>
+			</div></a>
+			<div class="relative box-content h-80 w-80 p-8 bg-slate-200 rounded-lg mt-20 z-0">
+				<div
+					class="absolute m-auto h-56 w-full p-8 bg-slate-900 rounded-lg z-10"
+					style="top: 1rem; right:-1rem;"
+				/>
+				<p class="absolute z-10 top-64 text-lg"><i>Coming Soon™…</i></p>
+			</div>
+		</div>
+	</div>
 </section>
-
-
