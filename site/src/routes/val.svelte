220a5dcdd503e0864e0ec786bb7e4c71f7bcecee
pyxiis <47072520+eniraa@users.noreply.github.com>
2022-04-09 12:11:18 -0400

    feat: add landing page

---
+++ site/src/routes/val.svelte
@@ -0,0 +1,15 @@
+<div class="bg-gradient-to-r from-cyan-500 to-blue-500 ..."><h1>Welcome to Val!</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style>
+    @font-face {
+        font-family: 'Ubuntu';
+        font-style: normal;
+        font-weight: 400;
+        src: url(/Ubuntu-th.ttf) format('ttf');
+    }
+    h1 {
+        font-family: Ubuntu
+    }
+</style>
